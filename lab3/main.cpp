47ae7af593cfd8dc444677c11a07da8d836b4c76
Kuleshova Viktoria <113125919+toryya@users.noreply.github.com>
2023-03-24 22:29:41 +0300

    Add files via upload

---
+++ lab3/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "Array.h"
+#include "Triangle.h"
+
+void First() {
+    std::cout << std::endl;
+    std::cout << "    Task 1:" << std::endl;
+    Triangle First, Second;
+    std::cin >> First >> Second;
+    if (First < Second)
+        std::cout << "First triangle is smaler than second" << std::endl;
+    if (First > Second)
+        std::cout << "First triangle is larger than second" << std::endl;
+    if (First == Second)
+        std::cout << "Triangles are same" << std::endl;
+    if (First != Second)
+        std::cout << "Triangles are different" << std::endl;
+    std::cout << std::endl;
+    std::cout << First << std::endl;
+    std::cout << std::endl;
+    std::cout << Second << std::endl;
+}
+
+void Second() {
+    std::cout << std::endl;
+    std::cout << "Task 2:" << std::endl;
+    Array First, Second;
+    std::cin >> First >> Second;
+    if (First < Second)
+        std::cout << "First array is smaler than second" << std::endl;
+    if (First > Second)
+        std::cout << "First array is larger than second" << std::endl;
+    if (First == Second)
+        std::cout << "Arrays are same" << std::endl;
+    if (First != Second)
+        std::cout << "Arrays are different" << std::endl;
+    std::cout << std::endl;
+    std::cout << First << std::endl;
+    std::cout << std::endl;
+    std::cout << Second << std::endl;
+    std::cout << std::endl;
+    Array Third = First.operator+(Second);
+    std::cout << Third << std::endl;
+}
+int main() {
+    First();
+    std::cout << "-----------------------------" ;
+    Second();
+    return 0;
+}
