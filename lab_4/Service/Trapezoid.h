5b79ab70731cc20e423ef9f1af18f2329c5d037c
Kuleshova Viktoria <113125919+toryya@users.noreply.github.com>
2023-04-27 17:10:27 +0300

    Add files via upload

---
+++ lab_4/Service/Trapezoid.h
@@ -0,0 +1,22 @@
+//
+// Created by ASUS on 18.03.2023.
+//
+
+#include <cstdio>
+#include "../Menu.h"
+
+class Trapezoid : public BaseCObject {
+public:
+    void initFromDialog() ;
+    CVector2D position() const;
+    double mass() const ;
+    double square() const ;
+    double perimeter() const ;
+    void draw() const ;
+    const char* classname() const ;
+    size_t size() const ;
+private:
+    double edge1, edge2, base1, base2, h;
+    CVector2D center;
+    double _mass;
+};
