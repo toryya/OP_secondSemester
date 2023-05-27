19d579e3fb86fe1c5496e0091e6bfb1ff3981cb1
Kuleshova Viktoria <113125919+toryya@users.noreply.github.com>
2023-05-27 13:31:55 +0300

    Add files via upload

---
+++ сube/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include "Cube.h"
+#include <ctime>
+using namespace std;
+#define Plane vector<vector<MiniCube*>>
+#define RightCenter RightPlane[1][1]->RightColor()
+#define LeftCenter LeftPlane[1][1]->LeftColor()
+#define UpCenter UpPlane[1][1]->UpColor()
+#define DownCenter DownPlane[1][1]->DownColor()
+#define FrontCenter FrontPlane[1][1]->FrontColor()
+#define BackCenter BackPlane[1][1]->BackColor()
+
+int main() {
+    CubeRubik Cube(time(0));
+    Cube.ReadCubeRubik(inputStream);
+    outputStream << "Изначальное состояние кубика:\n\n";
+    Cube.PrintCubeRubik(outputStream);
+    outputStream << "\n\nШаги разборки:\n\n";
+    Cube.Shuffle();
+    outputStream << "\n\nСостояние кубика после разборки:\n\n";
+    Cube.PrintCubeRubik(outputStream);
+    outputStream << "\n\nШаги сборки для первого кубика:\n\n";
+    Cube.FindSolution();
+
+    outputStream << "\n\nКубик после 7ми шагов сборки:\n\n";
+    (Cube.isCubeCompleted()) ? outputStream << "\tКубик успешно собран\n\n" :
+    outputStream << "К сожалению, кубик не собран, прошу понять и простить\n\n";Cube.PrintCubeRubik(outputStream);
+    return 0;
+}
